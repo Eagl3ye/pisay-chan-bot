77ab6a840aa958cdb66c411b9b4c51c6a6ca4f3e
Eagl3ye <vincejaphethloable@gmail.com>
2020-10-04 23:16:46 +0800

    Update coding.py

--- cogs/coding.py
+++ cogs/coding.py
@@ -9,7 +9,7 @@ class Coding(commands.Cog):
 	@commands.Cog.listener()
 	async def on_message(self, message):
 		msg = message
-		if message.channel.id == 762322266116980786:
+		if message.channel.id == 762322266116980786 and (message.content[0:4] == "```py" or message.content[0:8] == "```python"):
 			print("<#762322266116980786>:")
 			print(message.content)
 		#await msg.delete()
